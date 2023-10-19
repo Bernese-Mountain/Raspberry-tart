125ed0dac21d7ce7b0d71c727914aae56725c73e
‘caoyue’ <1310235550@qq.com>
2023-10-19 15:28:31 +0800

    #update 页面更新

---
+++ packages/raspberry-admin/src/views/dashboard/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="dashboard">
+        <h1>Dashboard</h1>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import axios from 'axios';
+import { onMounted } from 'vue';
+onMounted(() => {
+  //实际开发过程中建议对api进行封装 搭配async/await使用
+  axios
+    .get(`/api/getUsers`)
+    .then((res) => {
+     console.log("res  ----->  ", res.data.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+});
+
+</script>
